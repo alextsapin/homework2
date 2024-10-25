7ef6dab1d057e32cf451fd324d53c32ea249f16b
alextsapin <96056751+alextsapin@users.noreply.github.com>
2024-10-25 12:25:13 +0200

    Add spinner

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -23,7 +23,7 @@ const HW10 = () => {
 
         setTimeout(() => {
             dispatch(loadingAC(false))
-        }, 1500)
+        }, 3000)
     }
 
     return (
--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,5 @@
-import s from './Loader.module.css'
+import s from './Loader.module.css';
+import spinner from './spinner.png';
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <img src={spinner} className={s.loader} alt="spinner"/>
+
